e285a4582507afb4b9ed16c27f09c0cb07052ac1
bhavnap01 <59989687+bhavnap01@users.noreply.github.com>
2025-05-23 16:12:15 +0530

    Add files via upload

---
+++ wiki_test.py
@@ -0,0 +1,12 @@
+import pytest
+from playwright.sync_api import sync_playwright
+
+def test_wiki():
+     with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        context = browser.new_context()
+        page = context.new_page()
+        page.goto("https://auth.wikimedia.org/enwiki/wiki/Special:UserLogin?useformat=desktop&usesul3=1&returnto=Main_Page&centralauthLoginToken=47c07ad64c196bdeb4b9a1a18e829cdb")
+        
+        assert "Log in - Wikipedia" in page.title()
+        browser.close()
