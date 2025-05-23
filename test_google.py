94d3334ecf9f700378385bac0c787da034f07bfd
bhavnap01 <59989687+bhavnap01@users.noreply.github.com>
2025-05-23 16:10:52 +0530

    Update test_google.py

--- test_google.py
+++ test_google.py
@@ -4,7 +4,7 @@ from playwright.sync_api import sync_playwright
 #pytest with playwright
 def test_google():
      with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=True)
         context = browser.new_context()
         page = context.new_page()
         page.goto("https://www.google.com")
